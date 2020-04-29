4091a9fe363fc0e4bc88c2f6113bdebcb48be929
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-04-29 12:10:43 +0800

    Update wheels.c

--- WHEELS/wheels.c
+++ WHEELS/wheels.c
@@ -16,3 +16,25 @@ int decrypt(int cipherText, int key)
 {
     return cipherText ^ key;
 }
+
+
+long long convertDecimalToBinary(int n)
+{
+    long long binaryNumber = 0;
+    int remainder, 
+        quotient,
+        i = 1, 
+        step = 1;
+
+    while (n != 0)
+    {
+        remainder = n % 2;
+        quotient = n / 2;
+        printf("Step %d: %d/2, 余数 = %d, 商 = %d\n", step++, n, remainder, quotient );
+        n = quotient;
+        binaryNumber += remainder * i;
+        i *= 10;
+    }
+    return binaryNumber;
+}
+
