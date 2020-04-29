6e6bb4c885f5002b7133c42b4db7462734973069
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-04-29 11:59:32 +0800

    Update First.c

--- First.c
+++ First.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>  // like import module in python & java
 
 int main( void )
 {
     printf("Hello, future!");
-    return 0;
+    return 0;                  // Return a value to OS
 }
