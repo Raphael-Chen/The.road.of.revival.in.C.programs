82279f475f9833259953edbcad5b07fb10c37792
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-10-22 10:39:21 +0800

    Update NO_PRINT.c
    Add comments for code

--- ABC/NO_PRINT.c
+++ ABC/NO_PRINT.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main( void )
- {
-   print ("This program does not link");
- }
+int main(void)
+{
+    print("This program does not link"); //should be use printf function not print
 
+    return 0;
+}
