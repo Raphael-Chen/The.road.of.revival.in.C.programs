801889cb77b1ffda4dabaa54353fe386a7f9951a
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-10-22 10:40:15 +0800

    Update NO_STDIO.c
    Add comment for code

--- ABC/NO_STDIO.c
+++ ABC/NO_STDIO.c
@@ -1,5 +1,8 @@
-int main( void )
- {
-   printf ("1001 C & C++ Tips");
- }
+int main(void)
+{
+    printf("1001 C & C++ Tips");
 
+    return 0;
+}
+
+//Attention, there is no head file from the beginning.
