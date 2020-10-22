a9a6b32a2d0552b229e57b16c699ae77498b4033
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-10-22 10:45:06 +0800

    Update USEBREAK.c
    Add comments for keyword

--- ABC/USEBREAK.c
+++ ABC/USEBREAK.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// keyword "break" 's function
 int main(void)
 {
     int counter;
@@ -7,7 +8,7 @@ int main(void)
     for (counter = 1; counter <= 100; counter++)
     {
         if (counter == 50)
-            break;
+            break;                       // break from current loop
 
         printf("%d ", counter);
     }
@@ -21,4 +22,6 @@ int main(void)
 
         printf("%d ", counter);
     }
+
+    return 0;
 }
