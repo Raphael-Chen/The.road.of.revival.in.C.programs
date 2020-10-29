ab1b16b8a3e0cb7d8b4d9d2970db9ec609754de0
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-10-29 11:12:39 +0800

    Update DO_MENU.c
    Add comments for code

--- ABC/DO_MENU.c
+++ ABC/DO_MENU.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 //[bug]the command CHKDSK in system function for windows!
+// do ...while循环结构语句的用法，输入字母q 或 Q程序退出。
 
 int main(void)
 {
@@ -11,7 +12,7 @@ int main(void)
     do
     {
         printf("A Display directory listing\n");
-        printf("B Display disk information\n");
+        // printf("B Display disk information\n");
         printf("C Change system date\n");
         printf("Q Quit\n");
         printf("Choice: ");
@@ -22,9 +23,11 @@ int main(void)
 
         if (letter == 'A')
             system("dir");
-        else if (letter == 'B')
-            system("CHKDSK");
+        // else if (letter == 'B')
+        //     system("CHKDSK");
         else if (letter == 'C')
             system("date");
     } while (letter != 'Q');
+
+    return 0;
 }
