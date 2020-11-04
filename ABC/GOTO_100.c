a6fb6765165132ee1263c42fa6c0a8bc26b0adbc
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-11-04 15:05:59 +0800

    Update GOTO_100.c
    Add comments for code.

--- ABC/GOTO_100.c
+++ ABC/GOTO_100.c
@@ -1,6 +1,7 @@
-//在某种条件下，goto语句可以替代for循环，但不宜滥用，容易破坏程序的逻辑
 #include <stdio.h>
 
+// 在某种条件下，goto语句可以替代for循环，但不宜滥用，容易破坏程序的逻辑
+// 在异常处理中经常被用到
 int main(void)
 {
     int count = 1;
@@ -9,5 +10,7 @@ label:
     printf("%d ", count++);
 
     if (count <= 100)
-        goto label;
+        goto label;        // Back to the label: and go on 
+
+    return 0;
 }
