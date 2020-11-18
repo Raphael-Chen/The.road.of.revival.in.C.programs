b78e222093801d1723a5122d36abaf82a0e6164c
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-11-18 11:32:08 +0800

    Update FOR_2VAR.c
    Add return expresion

--- ABC/FOR_2VAR.c
+++ ABC/FOR_2VAR.c
@@ -1,10 +1,13 @@
-//在for循环中使用一个以上的变量
 #include <stdio.h>
 
+
+//在for循环中使用一个以上的变量
 int main(void)
 {
     int i, j;
 
     for (i = 0, j = 100; i <= 100; i++, j++)
         printf("i = %d j = %d\n", i, j);
+
+    return 0;
 }
