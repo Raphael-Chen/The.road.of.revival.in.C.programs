5c985998687b96d519a376e9e168b317e34b7181
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-11-18 11:33:57 +0800

    Update ZERO_PAD.c
    Add comment and return expresion

--- ABC/ZERO_PAD.c
+++ ABC/ZERO_PAD.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Pad with zero from left of value
 int main(void)
 {
     int value = 5;
@@ -8,4 +9,7 @@ int main(void)
     printf("%02d\n", value);
     printf("%03d\n", value);
     printf("%04d\n", value);
+    printf("%15d\n", value); // 15 space to fill
+
+    return 0;
 }
