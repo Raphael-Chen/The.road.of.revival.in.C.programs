3c38cda1fed84ea6ca8ac0c9fc5b1d1bab1cd1b3
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-11-19 11:30:29 +0800

    Update cdpwd.c
    Replace  a not exist directory.

--- Platform_dependence/Like_Unix/filedir/cdpwd.c
+++ Platform_dependence/Like_Unix/filedir/cdpwd.c
@@ -1,14 +1,34 @@
 #include "apue.h"
 
+/***************
+ *   Figure 4.24. Example of getcwd function
+ * 
+ *   NAME
+ *       getcwd, getwd, get_current_dir_name - get current working directory
+ *
+ *   SYNOPSIS
+ *       #include <unistd.h>
+ *
+ *       char *getcwd(char *buf, size_t size);
+ *       char *getwd(char *buf);
+ *       char *get_current_dir_name(void);
+ * 
+ *       Returns: buf if OK, NULL on error
+ * 
+ ****************/
+
+// #define PATH "/usr/spool/uucppublic"
+#define PATH "/usr/bin"
+
 int main(void)
 {
     char *ptr;
     size_t size;
 
-    if (chdir("/usr/spool/uucppublic") < 0)
+    if (chdir( PATH ) < 0)
         err_sys("chdir failed");
 
-    ptr = path_alloc(&size); /* our own function */
+    ptr = path_alloc(&size);            /* our own function */
     if (getcwd(ptr, size) == NULL)
         err_sys("getcwd failed");
 
@@ -16,3 +36,4 @@ int main(void)
     
     exit(0);
 }
+
