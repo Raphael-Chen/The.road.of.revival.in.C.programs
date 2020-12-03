ac1835f5c7a183dab989b048fc7aa62fbcb5ecad
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-12-03 15:24:18 +0800

    Update FLT_FMT.c

--- ABC/FLT_FMT.c
+++ ABC/FLT_FMT.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// 浮点数精度控制输出
 int main(void)
 {
     float value = 1.23456;
@@ -7,4 +8,6 @@ int main(void)
     printf("%8.1f\n", value);
     printf("%8.3f\n", value);
     printf("%8.5f\n", value);
+
+    return 0;
 }
