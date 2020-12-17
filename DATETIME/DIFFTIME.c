abcf8348db854bfe9a9c75353bd4f13b7472a86f
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-12-17 11:41:15 +0800

    Update DIFFTIME.c
    Add more comments to explan code.

--- DATETIME/DIFFTIME.c
+++ DATETIME/DIFFTIME.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <time.h>
 
+/************
+ *   7.27.2.2 The difftime function
+ *   Synopsis
+ *
+ *   #include <time.h>
+ *   double difftime(time_t time1, time_t time0);
+ *
+ *   Description
+ *   The difftime function computes the difference between two calendar times: time1 -
+ *   time0.
+ *
+ *   Returns
+ *   The difftime function returns the difference expressed in seconds as a double.
+**************/
+
 int main(void)
 {
     time_t start_time;
@@ -10,10 +25,13 @@ int main(void)
 
     printf("About to delay 5 seconds\n");
 
+    // 优缺点与DELAY_5.c 相同
     do
     {
         time(&current_time);
     } while (difftime(current_time, start_time) < 5.0);
 
     printf("Done\n");
+
+    return 0;
 }
