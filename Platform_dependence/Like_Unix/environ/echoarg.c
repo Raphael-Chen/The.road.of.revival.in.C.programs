286860271ec8a907ab72a55a1af77901239465af
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-12-23 16:52:41 +0800

    Update echoarg.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/environ/echoarg.c
+++ Platform_dependence/Like_Unix/environ/echoarg.c
@@ -1,11 +1,21 @@
 #include "apue.h"
 
-int
-main(int argc, char *argv[])
-{
-	int		i;
 
-	for (i = 0; i < argc; i++)		/* echo all command-line args */
-		printf("argv[%d]: %s\n", i, argv[i]);
-	exit(0);
+// Figure 7.4. Echo all command-line arguments to standard output
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for (i = 0; i < argc; i++)           /* echo all command-line args */
+        printf("argv[%d]: %s\n", i, argv[i]);
+
+    exit(0);
 }
+
+
+// $ ./echoarg arg1 TEST foo
+// argv[0]: ./echoarg
+// argv[1]: arg1
+// argv[2]: TEST
+// argv[3]: foo
