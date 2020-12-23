856f326c616d0393de263b6f6250809d224d2db2
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2020-12-23 16:49:29 +0800

    Update hello1.c
    Add more comments.

--- Platform_dependence/Like_Unix/environ/hello1.c
+++ Platform_dependence/Like_Unix/environ/hello1.c
@@ -1,6 +1,28 @@
-#include	<stdio.h>
+#include <stdio.h>
 
+// Figure 7.1. Classic C program
 main()
 {
-	printf("hello, world\n");
+    printf("hello, world\n");
 }
+
+
+// When we compile and run the program in Figure 7.1, we see that the exit code is random. 
+// If we compile the same program on different systems, we are likely to get different exit codes, 
+// depending on the contents of the stack and register contents at the time that the 
+// main function returns:
+//
+// $ cc hello.c
+// $ ./a.out
+// hello, world
+// $ echo $?      # print the exit status
+// 13
+
+
+// Now if we enable the 1999 ISO C compiler extensions, we see that the exit code changes:
+// $ cc -std=c99 hello.c      # enable gcc's 1999 ISO C extensions
+// hello.c:4: warning: return type defaults to 'int'
+// $ ./a.out
+// hello, world
+// $ echo $?                 # role="italicAlt"print the exit status
+// 0
