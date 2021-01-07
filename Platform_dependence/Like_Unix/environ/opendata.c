78fd8efe5f3d79af81d817363cd0cd592c6d8ebd
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-01-07 11:47:13 +0800

    Format old style and add more comments.

--- Platform_dependence/Like_Unix/environ/opendata.c
+++ Platform_dependence/Like_Unix/environ/opendata.c
@@ -1,14 +1,31 @@
-#include	<stdio.h>
+#include <stdio.h>
 
-FILE *
-open_data(void)
+// Figure 7.14. Incorrect usage of an automatic variable
+
+// Figure 7.14 shows a function called open_data that opens
+// a standard I/O stream and sets the buffering for the stream.
+
+FILE *open_data(void)
 {
-	FILE	*fp;
-	char	databuf[BUFSIZ];  /* setvbuf makes this the stdio buffer */
+    FILE *fp;
+    char databuf[BUFSIZ]; /* setvbuf makes this the stdio buffer */
 
-	if ((fp = fopen("datafile", "r")) == NULL)
-		return(NULL);
-	if (setvbuf(fp, databuf, _IOLBF, BUFSIZ) != 0)
-		return(NULL);
-	return(fp);		/* error */
+    if ((fp = fopen("datafile", "r")) == NULL)
+        return (NULL);
+
+    if (setvbuf(fp, databuf, _IOLBF, BUFSIZ) != 0)
+        return (NULL);
+
+    return (fp);           /* error */
 }
+
+/************
+    The problem is that when open_data returns, the space it used on 
+    the stack will be used by the stack frame for the next function 
+    that is called. But the standard I/O library will still be using 
+    that portion of memory for its stream buffer. Chaos is sure to result. 
+    To correct this problem, the array databuf needs to be allocated 
+    from global memory, either statically ( static or extern ) or
+    dynamically (one of the alloc functions).
+*********/
+
