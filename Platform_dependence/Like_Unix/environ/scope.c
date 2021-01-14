f0f3f4a22999fb37feeaabe9af5aac9dfc8ff2a4
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-01-14 16:01:50 +0800

    Update scope.c
    Add some comments.

--- Platform_dependence/Like_Unix/environ/scope.c
+++ Platform_dependence/Like_Unix/environ/scope.c
@@ -1,14 +1,19 @@
-int
-f1(int val)
+// 7.10 Exercises
+// At the end of Section 7.10, we showed how a function can't return a pointer 
+// to an automatic variable. Is the following code correct?
+
+int f1(int val)
 {
-	int		num = 0;
-	int		*ptr = &num;
+    int num = 0;
+    int *ptr = &num;
 
-	if (val == 0) {
-		int		val;
+    if (val == 0)
+    {
+        int val;
 
-		val = 5;
-		ptr = &val;
-	}
-	return(*ptr + 1);
+        val = 5;
+        ptr = &val;
+    }
+    
+    return (*ptr + 1);
 }
