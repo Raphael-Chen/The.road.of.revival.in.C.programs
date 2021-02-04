7ec36918f99d3284d4b07e09f71841b59ce2113e
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-02-04 16:42:01 +0800

    Update echoall.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/proc/echoall.c
+++ Platform_dependence/Like_Unix/proc/echoall.c
@@ -1,17 +1,21 @@
 #include "apue.h"
 
-int
-main(int argc, char *argv[])
+// Figure 8.17. Echo all command-line arguments and all environment strings
+// The program echoall that is executed twice in the program in Figure 8.16 is 
+// shown in Figure 8.17. It is a trivial program that echoes all its
+// command-line arguments and its entire environment list.
+
+int main(int argc, char *argv[])
 {
-	int			i;
-	char		**ptr;
-	extern char	**environ;
+    int i;
+    char **ptr;
+    extern char **environ;
 
-	for (i = 0; i < argc; i++)		/* echo all command-line args */
-		printf("argv[%d]: %s\n", i, argv[i]);
+    for (i = 0; i < argc; i++)              /* echo all command-line args */
+        printf("argv[%d]: %s\n", i, argv[i]);
 
-	for (ptr = environ; *ptr != 0; ptr++)	/* and all env strings */
-		printf("%s\n", *ptr);
+    for (ptr = environ; *ptr != 0; ptr++)   /* and all env strings */
+        printf("%s\n", *ptr);
 
-	exit(0);
+    exit(0);
 }
