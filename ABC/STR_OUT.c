d603f8f3bcf2e5c52fbc63d3e8da9c6d915071c7
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-02-04 16:45:54 +0800

    Update STR_OUT.c
    Add comment and remove incorrect comment.

--- ABC/STR_OUT.c
+++ ABC/STR_OUT.c
@@ -1,9 +1,10 @@
-//错误范例
 #include <stdio.h>
 
 int main(void)
 {
     char title[255] = "Jamsa's 1001 C & C++ Tips";
 
-    printf("The name of this book is %s\n", title);
+    printf("The name of this book is %s\n", title);  // use %s to output strings
+
+    return 0;
 }
