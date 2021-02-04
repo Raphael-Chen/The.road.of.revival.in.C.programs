3228f4f48f2ad5fbc1d017a150d8954f07581c54
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-02-04 16:47:16 +0800

    Update PTR_OUT.c
    Add commnet for code.

--- ABC/PTR_OUT.c
+++ ABC/PTR_OUT.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+// How to output a address of the variable.(with %p in printf function)
 int main(void)
 {
     int value;
 
     printf("The address of the variable value is %p\n",
            &value);
+
+    return 0;
 }
