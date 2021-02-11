58e9ceffbde7e03fc78753d247f85279439161b9
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-02-11 15:23:22 +0800

    Update UPPERERR.c
    Add more comments.

--- ABC/UPPERERR.c
+++ ABC/UPPERERR.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-Main()
+// 编译错误：main首字母大写，C语言大小写严格
+// (.text+0x20): undefined reference to `main'
+int Main( void )
 {
     printf("This program does not compile.");
+
+    return 0;
 }
