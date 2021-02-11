2baf295ce5d432b2a01c43d94edc3a59ed24e72c
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-02-11 15:22:17 +0800

    Update exec2.c
    Modify the path of program and add more comments.

--- Platform_dependence/Like_Unix/proc/exec2.c
+++ Platform_dependence/Like_Unix/proc/exec2.c
@@ -1,19 +1,43 @@
 #include "apue.h"
 #include <sys/wait.h>
 
-int
-main(void)
-{
-	pid_t	pid;
+// Figure 8.20. A program that exec s an interpreter file
+// The program echoarg (the interpreter) just echoes each of its 
+// command-line arguments. (This is the program from Figure 7.4.)
+// "/home/sar/bin/testinterp"
+#define PATH "./testinterp"
 
-	if ((pid = fork()) < 0) {
-		err_sys("fork error");
-	} else if (pid == 0) {			/* child */
-		if (execl("/home/sar/bin/testinterp",
-				  "testinterp", "myarg1", "MY ARG2", (char *)0) < 0)
-			err_sys("execl error");
-	}
-	if (waitpid(pid, NULL, 0) < 0)	/* parent */
-		err_sys("waitpid error");
-	exit(0);
+int main(void)
+{
+    pid_t pid;
+
+    if ((pid = fork()) < 0)
+    {
+        err_sys("fork error");
+    }
+    else if (pid == 0)              /* child */
+    { 
+        if (execl( PATH, //"/home/sar/bin/testinterp",
+                  "testinterp", "myarg1", "MY ARG2", (char *)0) < 0)
+            err_sys("execl error");
+    }
+    if (waitpid(pid, NULL, 0) < 0) /* parent */
+        err_sys("waitpid error");
+
+    exit(0);
 }
+
+
+// $ ./testinterp
+// argv[0]: ./echoarg
+// argv[1]: foo
+// argv[2]: ./testinterp
+
+// $ ./exec2
+// argv[0]: ./echoarg
+// argv[1]: foo
+// argv[2]: ./testinterp
+// argv[3]: myarg1
+// argv[4]: MY ARG2
+
+
