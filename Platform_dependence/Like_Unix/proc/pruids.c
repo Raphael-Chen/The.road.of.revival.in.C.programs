4ba99150c3222294391b06babfdce0db49c1efcb
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-02-25 10:42:35 +0800

    Update pruids.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/proc/pruids.c
+++ Platform_dependence/Like_Unix/proc/pruids.c
@@ -1,8 +1,30 @@
 #include "apue.h"
 
-int
-main(void)
+
+// Figure 8.25. Print real and effective user IDs
+
+int main(void)
 {
-	printf("real uid = %d, effective uid = %d\n", getuid(), geteuid());
-	exit(0);
+    printf("real uid = %d, effective uid = %d\n", getuid(), geteuid());
+
+    exit(0);
 }
+
+
+// NAME
+//        getuid, geteuid - get user identity
+//
+// SYNOPSIS
+//        #include <unistd.h>
+//        #include <sys/types.h>
+//
+//        uid_t getuid(void);
+//        uid_t geteuid(void);
+//
+// DESCRIPTION
+//        getuid() returns the real user ID of the calling process.
+//
+//        geteuid() returns the effective user ID of the calling process.
+
+// ERRORS
+//        These functions are always successful.
