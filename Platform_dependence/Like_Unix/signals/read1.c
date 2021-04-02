2f320daa71c30e0bff33fc0dbe79f43c1cf0f9e3
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-04-02 11:10:10 +0800

    Update read1.c
    Format old style and add more comments

--- Platform_dependence/Like_Unix/signals/read1.c
+++ Platform_dependence/Like_Unix/signals/read1.c
@@ -1,27 +1,34 @@
 #include "apue.h"
 
-static void	sig_alrm(int);
+// Figure 10.10 Calling read with a timeout
+static void sig_alrm(int);
 
-int
-main(void)
+int main(void)
 {
-	int		n;
-	char	line[MAXLINE];
+    int n;
+    char line[MAXLINE];
 
-	if (signal(SIGALRM, sig_alrm) == SIG_ERR)
-		err_sys("signal(SIGALRM) error");
+    if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+        err_sys("signal(SIGALRM) error");
 
-	alarm(10);
-	if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
-		err_sys("read error");
-	alarm(0);
+    alarm(10);
+    if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
+        err_sys("read error");
+    alarm(0);
 
-	write(STDOUT_FILENO, line, n);
-	exit(0);
+    write(STDOUT_FILENO, line, n);
+    
+    exit(0);
 }
 
-static void
-sig_alrm(int signo)
+static void sig_alrm(int signo)
 {
-	/* nothing to do, just return to interrupt the read */
+    /* nothing to do, just return to interrupt the read */
 }
+
+// A common use for alarm, in addition to implementing the sleep function, is to put an
+// upper time limit on operations that can block. For example, if we have a read
+// operation on a device that can block (a ‘‘slow’’ device, as described in Section 10.5), we
+// might want the read to time out after some amount of time. The program in
+// Figure 10.10 does this, reading one line from standard input and writing it to standard
+// output.
