abc89bc07fe3b5b6bc8912ac2920f663b7ecc96c
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-04-09 21:03:33 +0800

    Update critical.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/signals/critical.c
+++ Platform_dependence/Like_Unix/signals/critical.c
@@ -1,45 +1,59 @@
 #include "apue.h"
 
-static void	sig_quit(int);
+// Figure 10.15 shows many of the signal features that we’ve been describing.
+static void sig_quit(int);
 
-int
-main(void)
+int main(void)
 {
-	sigset_t	newmask, oldmask, pendmask;
+    sigset_t newmask, oldmask, pendmask;
 
-	if (signal(SIGQUIT, sig_quit) == SIG_ERR)
-		err_sys("can't catch SIGQUIT");
+    if (signal(SIGQUIT, sig_quit) == SIG_ERR)
+        err_sys("can't catch SIGQUIT");
 
-	/*
+    /*
 	 * Block SIGQUIT and save current signal mask.
 	 */
-	sigemptyset(&newmask);
-	sigaddset(&newmask, SIGQUIT);
-	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
-		err_sys("SIG_BLOCK error");
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGQUIT);
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+        err_sys("SIG_BLOCK error");
 
-	sleep(5);	/* SIGQUIT here will remain pending */
+    sleep(5); /* SIGQUIT here will remain pending */
 
-	if (sigpending(&pendmask) < 0)
-		err_sys("sigpending error");
-	if (sigismember(&pendmask, SIGQUIT))
-		printf("\nSIGQUIT pending\n");
+    if (sigpending(&pendmask) < 0)
+        err_sys("sigpending error");
+    if (sigismember(&pendmask, SIGQUIT))
+        printf("\nSIGQUIT pending\n");
 
-	/*
+    /*
 	 * Restore signal mask which unblocks SIGQUIT.
 	 */
-	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-		err_sys("SIG_SETMASK error");
-	printf("SIGQUIT unblocked\n");
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+        err_sys("SIG_SETMASK error");
+    printf("SIGQUIT unblocked\n");
 
-	sleep(5);	/* SIGQUIT here will terminate with core file */
-	exit(0);
+    sleep(5); /* SIGQUIT here will terminate with core file */
+
+    exit(0);
 }
 
-static void
-sig_quit(int signo)
+static void sig_quit(int signo)
 {
-	printf("caught SIGQUIT\n");
-	if (signal(SIGQUIT, SIG_DFL) == SIG_ERR)
-		err_sys("can't reset SIGQUIT");
+    printf("caught SIGQUIT\n");
+    if (signal(SIGQUIT, SIG_DFL) == SIG_ERR)
+        err_sys("can't reset SIGQUIT");
 }
+
+
+// $ ./critical 
+// ^\                    // generate signal once (before 5 seconds are up)
+// SIGQUIT pending
+// caught SIGQUIT
+// SIGQUIT unblocked
+// ^\Quit
+// $ ./critical 
+// ^\^\^\^\^\^\^\^\^\^\  // generate signal 10 times (before 5 seconds are up)
+// SIGQUIT pending
+// caught SIGQUIT
+// SIGQUIT unblocked
+// ^\Quit
