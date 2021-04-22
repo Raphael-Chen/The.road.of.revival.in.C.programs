675739fde4b09f393a2378b4b7b618be03d88936
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-04-22 16:42:43 +0800

    Update linked_list.h
    Abstract data type.

--- Algorithms/linked_list.h
+++ Algorithms/linked_list.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /***
     A linked list is a data structure in which the objects are arranged in a linear order.
@@ -8,30 +9,33 @@
  **/
 
 #define LIST_BEGAIN    printf("\n[ ")
-#define LIST_END       printf("]\n")
+#define LIST_END       printf("]\n\n")
+typedef int _TYPE;     // 抽象出元素数据类型
+
 
 // 单向链表数据结构的基本实现
 typedef struct Link
 {
-    int elem;
+    _TYPE elem;
     // ... more elements
     
     struct Link *next;
 } link;
 
-link *create_list( int n );
+link *create_list( _TYPE n );
 
 //链表插入的函数，p是链表，elem是插入的结点的数据域，add是插入的位置
-link *insertElem(link *p, int elem, int add);
+link *insertElem(link *list, _TYPE elem, _TYPE add);
 
 //删除结点的函数，p代表操作链表，add代表删除节点的位置
-link *delElem(link *p, int add);
+link *delElem(link *list, _TYPE add);
 
 //查找结点的函数，elem为目标结点的数据域的值
-int selectElem(link *p, int elem);
+_TYPE selectElem(link *list, _TYPE elem);
 
 //更新结点的函数，newElem为新的数据域的值
-link *amendElem(link *p, int add, int newElem);
+link *amendElem(link *list, _TYPE add, _TYPE newElem);
 
 // 遍历并打印出所有节点的内容
-void display(link *p);
+bool display(link *list);
+
