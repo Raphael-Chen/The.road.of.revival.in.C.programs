1c9415a4f281300b0d99fd380afd102b4ff8d7a8
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-04-22 16:34:09 +0800

    Update suspend1.c
    Format old stytle.

--- Platform_dependence/Like_Unix/signals/suspend1.c
+++ Platform_dependence/Like_Unix/signals/suspend1.c
@@ -1,56 +1,65 @@
 #include "apue.h"
 
-static void	sig_int(int);
+// Figure 10.22 Protecting a critical region from a signal
+static void sig_int(int);
 
-int
-main(void)
+int main(void)
 {
-	sigset_t	newmask, oldmask, waitmask;
+    sigset_t newmask, oldmask, waitmask;
 
-	pr_mask("program start: ");
+    pr_mask("program start: ");
 
-	if (signal(SIGINT, sig_int) == SIG_ERR)
-		err_sys("signal(SIGINT) error");
-	sigemptyset(&waitmask);
-	sigaddset(&waitmask, SIGUSR1);
-	sigemptyset(&newmask);
-	sigaddset(&newmask, SIGINT);
+    if (signal(SIGINT, sig_int) == SIG_ERR)
+        err_sys("signal(SIGINT) error");
+    sigemptyset(&waitmask);
+    sigaddset(&waitmask, SIGUSR1);
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGINT);
 
-	/*
+    /*
 	 * Block SIGINT and save current signal mask.
 	 */
-	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
-		err_sys("SIG_BLOCK error");
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+        err_sys("SIG_BLOCK error");
 
-	/*
+    /*
 	 * Critical region of code.
 	 */
-	pr_mask("in critical region: ");
+    pr_mask("in critical region: ");
 
-	/*
+    /*
 	 * Pause, allowing all signals except SIGUSR1.
 	 */
-	if (sigsuspend(&waitmask) != -1)
-		err_sys("sigsuspend error");
+    if (sigsuspend(&waitmask) != -1)
+        err_sys("sigsuspend error");
 
-	pr_mask("after return from sigsuspend: ");
+    pr_mask("after return from sigsuspend: ");
 
-	/*
+    /*
 	 * Reset signal mask which unblocks SIGINT.
 	 */
-	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-		err_sys("SIG_SETMASK error");
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+        err_sys("SIG_SETMASK error");
 
-	/*
+    /*
 	 * And continue processing ...
 	 */
-	pr_mask("program exit: ");
+    pr_mask("program exit: ");
 
-	exit(0);
+    exit(0);
 }
 
-static void
-sig_int(int signo)
+static void sig_int(int signo)
 {
-	pr_mask("\nin sig_int: ");
+    pr_mask("\nin sig_int: ");
 }
+
+
+// $ ./suspend1
+// program start:
+// in critical region:  SIGINT
+// ^C                              # type the interrupt character
+// in sig_int:  SIGINT SIGUSR1
+// after return from sigsuspend:  SIGINT
+// program exit:
+
