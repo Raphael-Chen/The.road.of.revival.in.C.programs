6843b2230b75f25249665e8a22c47834931ceb77
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-04-22 16:40:57 +0800

    Update linked_list.c
    Add init_empty function and rename some variables.

--- Algorithms/linked_list.c
+++ Algorithms/linked_list.c
@@ -6,45 +6,60 @@
 // - 链表中结点之间采用指针进行链接，当对链表中的数据元素实行插入或者删除操作时，只需要改变指针的
 // 指向，无需像顺序表那样移动插入或删除位置的后续元素，简单快捷。
 
-link *create_list(int n)
+//
+link* init_empty( link* head )
 {
-    link *p = (link *)malloc(sizeof(link)); //创建一个头结点
-    link *temp = p;                         //声明一个指针指向头结点，用于遍历链表
+    head ->elem = 0;
+    //  ... more elements init
+    head ->next = NULL;
+
+    return head;
+}
+
+link *create_list(_TYPE n)
+{
+    link *list = (link *)malloc(sizeof(link)); //创建一个头结点
+    link *temp = list;                         //声明一个指针指向头结点，用于遍历链表
+    _TYPE i;
 
     //生成链表
-    for (int i = 1; i < n + 1; i++)
+    for ( i = 1; i < n + 1; i++ )
     {
-        link *a = (link *)malloc(sizeof(link));
-        a->elem = i;
-        a->next = NULL;
+        link *a    = (link *)malloc(sizeof(link));
+        a->elem    = i;
+        a->next    = NULL;
         temp->next = a;
         temp = temp->next;
     }
 
-    return p;
+    return list;
 }
 
-link *insertElem(link *p, int elem, int add)
+link *insertElem(link *list, _TYPE elem, _TYPE add)
 {
-    link *temp = p; //创建临时结点temp
+    link *temp = list; //创建临时结点temp
+    _TYPE i;
+
     //首先找到要插入位置的上一个结点
-    for (int i = 1; i < add; i++)
+    for ( i = 1; i < add; i++ )
     {
         if (temp == NULL)
         {
             printf("插入位置无效\n");
-            return p;
+            return list;
         }
         temp = temp->next;
     }
+
     //创建插入结点c
     link *c = (link *)malloc(sizeof(link));
     c->elem = elem;
+
     //向链表中插入结点
     c->next = temp->next;
     temp->next = c;
 
-    return p;
+    return list;
 }
 
 /** fake code
@@ -56,11 +71,13 @@ link *insertElem(link *p, int elem, int add)
     5   x.next.pre = x.pre
 **/
 
-link *delElem(link *p, int add)
+link *delElem(link *list, _TYPE add)
 {
-    link *temp = p;
+    link *temp = list;
+    _TYPE i;
+
     //遍历到被删除结点的上一个结点
-    for (int i = 1; i < add; i++)
+    for ( i = 1; i < add; i++ )
     {
         temp = temp->next;
     }
@@ -68,50 +85,54 @@ link *delElem(link *p, int add)
     temp->next = temp->next->next; //删除某个结点的方法就是更改前一个结点的指针域
     free(del);                     //手动释放该结点，防止内存泄漏
 
-    return p;
+    return list;
 }
 
 // 链表中查找某结点
-int selectElem(link *p, int elem)
+_TYPE selectElem(link *list, _TYPE elem)
 {
-    link *t = p;
-    int i = 1;
-    while (t->next)
+    link *shadow = list;
+    _TYPE i = 1;
+
+    while (shadow->next)
     {
-        t = t->next;
-        if (t->elem == elem)
+        shadow = shadow->next;
+        if (shadow->elem == elem)
         {
             return i;
         }
         i++;
     }
+    
     return -1;
 }
 
-link *amendElem(link *p, int add, int newElem)
+
+link *amendElem(link *list, _TYPE add, _TYPE newElem)
 {
-    link *temp = p;
-    temp = temp->next; //tamp指向首元结点
+    link *temp = list;
+    temp = temp->next;          //temp指向首元结点
     //temp指向被删除结点
-    for (int i = 1; i < add; i++)
+    for (_TYPE i = 1; i < add; i++)
     {
         temp = temp->next;
     }
     temp->elem = newElem;
 
-    return p;
+    return list;
 }
 
 
 // 打印出链表中所有的元素
-void display(link *p)
+bool display(link *list)
 {
-    if ( NULL == p )
+    if ( NULL == list )
     {
-        return NULL;
+        printf( "链表为空，停止打印\n" );
+        return false;
     }
 
-    link *temp = p; //将temp指针重新指向头结点
+    link *temp = list; //将temp指针重新指向头结点
     //只要temp指针指向的结点的next不是Null，就执行输出语句。
 
     LIST_BEGAIN;
@@ -122,6 +143,8 @@ void display(link *p)
     }
     LIST_END;
     // printf("\n");
+
+    return true;
 }
 
 // http.//data.biancheng.net/view/5.html
@@ -130,19 +153,19 @@ int main(void)
 {
     //初始化链表（1，2，3，4）
     printf("初始化链表为：\n");
-    link *p = create_list(4);
-    display(p);
+    link *list = create_list(4);
+    display(list);
 
     printf("在第4的位置插入元素5：\n");
-    p = insertElem(p, 5, 4);
-    display(p);
+    list = insertElem(list, 5, 4);
+    display(list);
 
     printf("删除元素3.\n");
-    p = delElem(p, 3);
-    display(p);
+    list = delElem(list, 3);
+    display(list);
 
     printf("查找元素2的位置为：\n");
-    int address = selectElem(p, 2);
+    _TYPE address = selectElem(list, 2);
     if (address == -1)
     {
         printf("没有该元素");
@@ -152,8 +175,8 @@ int main(void)
         printf("元素2的位置为：%d\n", address);
     }
     printf("更改第3的位置的数据为7.\n");
-    p = amendElem(p, 3, 7);
-    display(p);
+    list = amendElem(list, 3, 7);
+    display(list);
 
     return 0;
 }
