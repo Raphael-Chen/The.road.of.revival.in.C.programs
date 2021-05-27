1d5653843a78f896849b3d730a6baef5e212c7e8
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-05-27 15:09:54 +0800

    Update maketimeout.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/threads/maketimeout.c
+++ Platform_dependence/Like_Unix/threads/maketimeout.c
@@ -1,15 +1,23 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
-void
-maketimeout(struct timespec *tsp, long minutes)
-{
-	struct timeval now;
 
-	/* get the current time */
-	gettimeofday(&now, NULL);
-	tsp->tv_sec = now.tv_sec;
-	tsp->tv_nsec = now.tv_usec * 1000; /* usec to nsec */
-	/* add the offset to get timeout value */
-	tsp->tv_sec += minutes * 60;
+// We can use the clock_gettime function (Section 6.10) to get the current time
+// expressed as a timespec structure. However, this function is not yet supported on all
+// platforms. Alternatively, we can use the gettimeofday function to get the current
+// time expressed as a timeval structure and translate it into a timespec structure. To
+// obtain the absolute time for the timeout value, we can use the following function
+// (assuming the maximum time blocked is expressed in minutes):
+
+void maketimeout(struct timespec *tsp, long minutes)
+{
+    struct timeval now;
+
+    /* get the current time */
+    gettimeofday(&now, NULL);
+    tsp->tv_sec = now.tv_sec;
+    tsp->tv_nsec = now.tv_usec * 1000; /* usec to nsec */
+    
+    /* add the offset to get timeout value */
+    tsp->tv_sec += minutes * 60;
 }
