5eb19fa31dcf4553a32368e407fec35d40c19096
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-05-27 15:02:33 +0800

    Update timedlock.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/threads/timedlock.c
+++ Platform_dependence/Like_Unix/threads/timedlock.c
@@ -1,31 +1,43 @@
 #include "apue.h"
 #include <pthread.h>
 
-int
-main(void)
-{
-	int err;
-	struct timespec tout;
-	struct tm *tmp;
-	char buf[64];
-	pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+// Figure 11.13 Using pthread_mutex_timedlock
+// In Figure 11.13, we see how to use pthread_mutex_timedlock to avoid blocking
+// indefinitely.
 
-	pthread_mutex_lock(&lock);
-	printf("mutex is locked\n");
-	clock_gettime(CLOCK_REALTIME, &tout);
-	tmp = localtime(&tout.tv_sec);
-	strftime(buf, sizeof(buf), "%r", tmp);
-	printf("current time is %s\n", buf);
-	tout.tv_sec += 10;	/* 10 seconds from now */
-	/* caution: this could lead to deadlock */
-	err = pthread_mutex_timedlock(&lock, &tout);
-	clock_gettime(CLOCK_REALTIME, &tout);
-	tmp = localtime(&tout.tv_sec);
-	strftime(buf, sizeof(buf), "%r", tmp);
-	printf("the time is now %s\n", buf);
-	if (err == 0)
-		printf("mutex locked again!\n");
-	else
-		printf("can't lock mutex again: %s\n", strerror(err));
-	exit(0);
+int main(void)
+{
+    int err;
+    struct timespec tout;
+    struct tm *tmp;
+    char buf[64];
+    pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+    pthread_mutex_lock(&lock);
+    printf("mutex is locked\n");
+
+    clock_gettime(CLOCK_REALTIME, &tout);
+    tmp = localtime(&tout.tv_sec);
+    strftime(buf, sizeof(buf), "%r", tmp);
+    printf("current time is %s\n", buf);
+    tout.tv_sec += 10;                     // 10 seconds from now 
+
+    // caution: this could lead to deadlock 
+    err = pthread_mutex_timedlock(&lock, &tout);
+    clock_gettime(CLOCK_REALTIME, &tout);
+    tmp = localtime(&tout.tv_sec);
+    strftime(buf, sizeof(buf), "%r", tmp);
+    printf("the time is now %s\n", buf);
+
+    if (err == 0)
+        printf("mutex locked again!\n");
+    else
+        printf("can't lock mutex again: %s\n", strerror(err));
+
+    exit(0); 
 }
+
+
+// This program deliberately locks a mutex it already owns to demonstrate how
+// pthread_mutex_timedlock works. This strategy is not recommended in practice,
+// because it can lead to deadlock.
