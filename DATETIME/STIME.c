a4117ccc5ef44f0f76a923a881c720b9fc0f0ea1
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-06-11 11:20:37 +0800

    Update STIME.c
    Add more comments.

--- DATETIME/STIME.c
+++ DATETIME/STIME.c
@@ -13,11 +13,38 @@ int main(void)
     if ( -1 != stime(&seconds))
     {
         printf( "stime is success~\n" );
-        /* code */
-    }else
+    }
+    else
     {
         perror("Set time fail: ");  //Operation not permitted
     }
     
-    
 }
+
+/****************
+    NAME
+        stime - set time  // Not belong to ISO C
+
+    SYNOPSIS
+        #include <time.h>
+
+        int stime(time_t *t);
+
+    Feature Test Macro Requirements for glibc (see feature_test_macros(7)):
+
+        stime(): _SVID_SOURCE
+
+    DESCRIPTION
+        stime()  sets  the system's idea of the time and date.  The time, pointed to by t, is mea‐
+        sured in seconds since the Epoch, 1970-01-01 00:00:00 +0000 (UTC).  stime()  may  be  exe‐
+        cuted only by the superuser.
+
+    RETURN VALUE
+        On success, zero is returned.  On error, -1 is returned, and errno is set appropriately.
+
+    ERRORS
+        EFAULT Error in getting information from user space.
+
+        EPERM  The calling process has insufficient privilege.  Under Linux the CAP_SYS_TIME priv‐
+                ilege is required.
+*****************/
