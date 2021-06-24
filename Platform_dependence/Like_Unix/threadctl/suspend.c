cc30366dd314089d275c204e94fee45fa6731db7
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-06-24 16:01:41 +0800

    Update suspend.c
    Format old styles and add more comments.

--- Platform_dependence/Like_Unix/threadctl/suspend.c
+++ Platform_dependence/Like_Unix/threadctl/suspend.c
@@ -1,67 +1,86 @@
 #include "apue.h"
 #include <pthread.h>
 
+// Figure 12.16 Synchronous signal handling
+
 int			quitflag;	/* set nonzero by thread */
 sigset_t	mask;
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock   = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t waitloc = PTHREAD_COND_INITIALIZER;
 
-void *
-thr_fn(void *arg)
+void *thr_fn(void *arg)
 {
-	int err, signo;
+    int err, signo;
 
-	for (;;) {
-		err = sigwait(&mask, &signo);
-		if (err != 0)
-			err_exit(err, "sigwait failed");
-		switch (signo) {
-		case SIGINT:
-			printf("\ninterrupt\n");
-			break;
+    for (;;)
+    {
+        err = sigwait(&mask, &signo);
+        if (err != 0)
+            err_exit(err, "sigwait failed");
+        switch (signo)
+        {
+            case SIGINT:
+                printf("\ninterrupt\n");
+                break;
 
-		case SIGQUIT:
-			pthread_mutex_lock(&lock);
-			quitflag = 1;
-			pthread_mutex_unlock(&lock);
-			pthread_cond_signal(&waitloc);
-			return(0);
+            case SIGQUIT:
+                pthread_mutex_lock(&lock);
+                quitflag = 1;
+                pthread_mutex_unlock(&lock);
+                pthread_cond_signal(&waitloc);
+                return (0);
 
-		default:
-			printf("unexpected signal %d\n", signo);
-			exit(1);
-		}
-	}
+            default:
+                printf("unexpected signal %d\n", signo);
+                exit(1);
+        }
+    }
 }
 
-int
-main(void)
+int main(void)
 {
-	int			err;
-	sigset_t	oldmask;
-	pthread_t	tid;
+    int err;
+    sigset_t oldmask;
+    pthread_t tid;
 
-	sigemptyset(&mask);
-	sigaddset(&mask, SIGINT);
-	sigaddset(&mask, SIGQUIT);
-	if ((err = pthread_sigmask(SIG_BLOCK, &mask, &oldmask)) != 0)
-		err_exit(err, "SIG_BLOCK error");
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    sigaddset(&mask, SIGQUIT);
+    if ((err = pthread_sigmask(SIG_BLOCK, &mask, &oldmask)) != 0)
+        err_exit(err, "SIG_BLOCK error");
 
-	err = pthread_create(&tid, NULL, thr_fn, 0);
-	if (err != 0)
-		err_exit(err, "can't create thread");
+    err = pthread_create(&tid, NULL, thr_fn, 0);
+    if (err != 0)
+        err_exit(err, "can't create thread");
 
-	pthread_mutex_lock(&lock);
-	while (quitflag == 0)
-		pthread_cond_wait(&waitloc, &lock);
-	pthread_mutex_unlock(&lock);
+    pthread_mutex_lock(&lock);
+    while (quitflag == 0)
+        pthread_cond_wait(&waitloc, &lock);
+    pthread_mutex_unlock(&lock);
 
-	/* SIGQUIT has been caught and is now blocked; do whatever */
-	quitflag = 0;
+    /* SIGQUIT has been caught and is now blocked; do whatever */
+    quitflag = 0;
 
-	/* reset signal mask which unblocks SIGQUIT */
-	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-		err_sys("SIG_SETMASK error");
-	exit(0);
+    /* reset signal mask which unblocks SIGQUIT */
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+        err_sys("SIG_SETMASK error");
+
+
+    exit(0);
 }
+
+// If we run this program, we get output similar to that from Figure 10.23:
+// $ ./a.out
+// ˆ?                          # type the interrupt character
+// interrupt
+// ˆ?                         # type the interrupt character again
+// interrupt
+// ˆ?                         # and again
+// interrupt
+// ˆ\ $                       # now terminate with quit character
+
+
+
+
+
