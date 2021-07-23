c5157bff4dba31362ef5e72bb91af18765d274e8
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-07-23 19:46:06 +0800

    Update setfd.c
    Add more comments.

--- Platform_dependence/Like_Unix/lib/setfd.c
+++ Platform_dependence/Like_Unix/lib/setfd.c
@@ -1,6 +1,7 @@
 #include "apue.h"
 #include <fcntl.h>
 
+// Figure 13.9 Set close-on-exec flag
 int set_cloexec(int fd)
 {
     int val;
@@ -8,7 +9,7 @@ int set_cloexec(int fd)
     if ((val = fcntl(fd, F_GETFD, 0)) < 0)
         return (-1);
 
-    val |= FD_CLOEXEC; /* enable close-on-exec */
+    val |= FD_CLOEXEC;                 // enable close-on-exec 
 
     return (fcntl(fd, F_SETFD, val));  // F_SETFD: Set file descriptor flags.
 }
