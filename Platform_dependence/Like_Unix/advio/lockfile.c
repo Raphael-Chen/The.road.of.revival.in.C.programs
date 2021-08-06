a5f032498ee5c779a21d73ba0337cbc45c2eac2f
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-08-06 16:04:14 +0800

    Update lockfile.c
    Format old styles and add more  comments .

--- Platform_dependence/Like_Unix/advio/lockfile.c
+++ Platform_dependence/Like_Unix/advio/lockfile.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int
-lockfile(int fd)
-{
-	struct flock fl;
 
-	fl.l_type = F_WRLCK;
-	fl.l_start = 0;
-	fl.l_whence = SEEK_SET;
-	fl.l_len = 0;
-	return(fcntl(fd, F_SETLK, &fl));
+// Figure 14.9 Place a write lock on an entire file
+int lockfile(int fd)
+{
+    struct flock fl;
+
+    fl.l_type = F_WRLCK;
+    fl.l_start = 0;
+    fl.l_whence = SEEK_SET;
+    fl.l_len = 0;
+
+    return (fcntl(fd, F_SETLK, &fl));
 }
