39e12d360e2c10e0dd17129da3d40d15beb7c1dc
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-08-20 11:22:31 +0800

    Update readn.c
    Format old styles.

--- Platform_dependence/Like_Unix/advio/readn.c
+++ Platform_dependence/Like_Unix/advio/readn.c
@@ -1,23 +1,30 @@
 #include "apue.h"
 
-ssize_t             /* Read "n" bytes from a descriptor  */
-readn(int fd, void *ptr, size_t n)
-{
-	size_t		nleft;
-	ssize_t		nread;
+// Figure 14.24 The readn and writen functions
 
-	nleft = n;
-	while (nleft > 0) {
-		if ((nread = read(fd, ptr, nleft)) < 0) {
-			if (nleft == n)
-				return(-1); /* error, return -1 */
-			else
-				break;      /* error, return amount read so far */
-		} else if (nread == 0) {
-			break;          /* EOF */
-		}
-		nleft -= nread;
-		ptr   += nread;
-	}
-	return(n - nleft);      /* return >= 0 */
+/* Read "n" bytes from a descriptor  */
+ssize_t readn(int fd, void *ptr, size_t n)
+{
+    size_t nleft;
+    ssize_t nread;
+
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ((nread = read(fd, ptr, nleft)) < 0)
+        {
+            if (nleft == n)
+                return (-1);      /* error, return -1 */
+            else
+                break;            /* error, return amount read so far */
+        }
+        else if (nread == 0)
+        {
+            break;                /* EOF */
+        }
+        nleft -= nread;
+        ptr += nread;
+    }
+
+    return (n - nleft); /* return >= 0 */
 }
