64d6a5246f0889ddb4f9d7407b81aab1b39b9497
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-08-20 11:23:15 +0800

    Update writen.c
    Format old styles and add more comments.

--- Platform_dependence/Like_Unix/advio/writen.c
+++ Platform_dependence/Like_Unix/advio/writen.c
@@ -1,23 +1,30 @@
 #include "apue.h"
 
-ssize_t             /* Write "n" bytes to a descriptor  */
-writen(int fd, const void *ptr, size_t n)
-{
-	size_t		nleft;
-	ssize_t		nwritten;
+// Figure 14.24 The readn and writen functions
 
-	nleft = n;
-	while (nleft > 0) {
-		if ((nwritten = write(fd, ptr, nleft)) < 0) {
-			if (nleft == n)
-				return(-1); /* error, return -1 */
-			else
-				break;      /* error, return amount written so far */
-		} else if (nwritten == 0) {
-			break;
-		}
-		nleft -= nwritten;
-		ptr   += nwritten;
-	}
-	return(n - nleft);      /* return >= 0 */
+/* Write "n" bytes to a descriptor  */
+ssize_t writen(int fd, const void *ptr, size_t n)
+{
+    size_t nleft;
+    ssize_t nwritten;
+
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ((nwritten = write(fd, ptr, nleft)) < 0)
+        {
+            if (nleft == n)
+                return (-1);               /* error, return -1 */
+            else
+                break;                     /* error, return amount written so far */
+        }
+        else if (nwritten == 0)
+        {
+            break;
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    
+    return (n - nleft);                     /* return >= 0 */
 }
