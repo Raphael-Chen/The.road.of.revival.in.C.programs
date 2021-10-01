c6836b7631a8bf0b54c83990f0fc8cd5196451fc
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-10-01 10:26:16 +0800

    Update myuclc.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/ipc1/myuclc.c
+++ Platform_dependence/Like_Unix/ipc1/myuclc.c
@@ -1,18 +1,26 @@
 #include "apue.h"
 #include <ctype.h>
 
-int
-main(void)
-{
-	int		c;
 
-	while ((c = getchar()) != EOF) {
-		if (isupper(c))
-			c = tolower(c);
-		if (putchar(c) == EOF)
-			err_sys("output error");
-		if (c == '\n')
-			fflush(stdout);
-	}
-	exit(0);
+// Figure 15.14 Filter to convert uppercase characters to lowercase
+// We compile this filter into the executable file myuclc, which we then invoke from
+// the program in Figure 15.15 using popen.
+
+int main( void )
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (isupper(c))
+            c = tolower(c);
+
+        if (putchar(c) == EOF)
+            err_sys("output error");
+
+        if (c == '\n')
+            fflush(stdout);
+    }
+
+    exit(0);
 }
