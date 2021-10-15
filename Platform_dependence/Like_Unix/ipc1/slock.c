6849b5720bb7d02ddd6c9ab0e1d040f3e1e8d3e4
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-10-15 20:12:35 +0800

    Update slock.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/ipc1/slock.c
+++ Platform_dependence/Like_Unix/ipc1/slock.c
@@ -4,48 +4,49 @@
 #include <unistd.h>
 #include <errno.h>
 
-struct slock *
-s_alloc()
-{
-	struct slock *sp;
-	static int cnt;
+// Figure 15.35 Mutual exclusion using a POSIX semaphore
 
-	if ((sp = malloc(sizeof(struct slock))) == NULL)
-		return(NULL);
-	do {
-		snprintf(sp->name, sizeof(sp->name), "/%ld.%d", (long)getpid(),
-		  cnt++);
-		sp->semp = sem_open(sp->name, O_CREAT|O_EXCL, S_IRWXU, 1);
-	} while ((sp->semp == SEM_FAILED) && (errno == EEXIST));
-	if (sp->semp == SEM_FAILED) {
-		free(sp);
-		return(NULL);
-	}
-	sem_unlink(sp->name);
-	return(sp);
+struct slock *s_alloc()
+{
+    struct slock *sp;
+    static int cnt;
+
+    if ((sp = malloc(sizeof(struct slock))) == NULL)
+        return (NULL);
+    do
+    {
+        snprintf(sp->name, sizeof(sp->name), "/%ld.%d", (long)getpid(),
+                 cnt++);
+        sp->semp = sem_open(sp->name, O_CREAT | O_EXCL, S_IRWXU, 1);
+    } while ((sp->semp == SEM_FAILED) && (errno == EEXIST));
+    
+    if (sp->semp == SEM_FAILED)
+    {
+        free(sp);
+        return (NULL);
+    }
+    sem_unlink(sp->name);
+
+    return (sp);
 }
 
-void
-s_free(struct slock *sp)
+void s_free(struct slock *sp)
 {
-	sem_close(sp->semp);
-	free(sp);
+    sem_close(sp->semp);
+    free(sp);
 }
 
-int
-s_lock(struct slock *sp)
+int s_lock(struct slock *sp)
 {
-	return(sem_wait(sp->semp));
+    return (sem_wait(sp->semp));
 }
 
-int
-s_trylock(struct slock *sp)
+int s_trylock(struct slock *sp)
 {
-	return(sem_trywait(sp->semp));
+    return (sem_trywait(sp->semp));
 }
 
-int
-s_unlock(struct slock *sp)
+int s_unlock(struct slock *sp)
 {
-	return(sem_post(sp->semp));
+    return (sem_post(sp->semp));
 }
