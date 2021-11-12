73351e1978750c2c55fd24a2cd646d23ec11e070
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-11-12 22:41:16 +0800

    Update clconn.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/sockets/clconn.c
+++ Platform_dependence/Like_Unix/sockets/clconn.c
@@ -1,29 +1,40 @@
 #include "apue.h"
 #include <sys/socket.h>
 
+// Figure 16.10 Connect with retry
+// Figure 16.10 shows one way to handle transient connect errors. These errors are likely
+// with a server that is running on a heavily loaded system.
+// There is a problem with the code shown in Figure 16.10: it isn’t portable.
+// This technique works on Linux and Solaris, but doesn’t work as expected on FreeBSD and
+// Mac OS X. If the first connection attempt fails, BSD-based socket implementations
+// continue to fail successive connection attempts when the same socket descriptor is used
+// with TCP.
+
 #define MAXSLEEP 128
 
-int
-connect_retry(int sockfd, const struct sockaddr *addr, socklen_t alen)
+int connect_retry(int sockfd, const struct sockaddr *addr, socklen_t alen)
 {
-	int numsec;
+    int numsec;
 
-	/*
+    /*
 	 * Try to connect with exponential backoff.
 	 */
-	for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1) {
-		if (connect(sockfd, addr, alen) == 0) {
-			/*
+    for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1)
+    {
+        if (connect(sockfd, addr, alen) == 0)
+        {
+            /*
 			 * Connection accepted.
 			 */
-			return(0);
-		}
+            return (0);
+        }
 
-		/*
+        /*
 		 * Delay before trying again.
 		 */
-		if (numsec <= MAXSLEEP/2)
-			sleep(numsec);
-	}
-	return(-1);
+        if (numsec <= MAXSLEEP / 2)
+            sleep(numsec);
+    }
+    
+    return (-1);
 }
