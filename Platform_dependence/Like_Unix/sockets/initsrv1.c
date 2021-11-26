3b9cbaec1dcc8994fcb96e65f10c15bf921b705d
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-11-26 22:49:49 +0800

    Update initsrv1.c
    Format old styles and add more comments.

--- Platform_dependence/Like_Unix/sockets/initsrv1.c
+++ Platform_dependence/Like_Unix/sockets/initsrv1.c
@@ -2,26 +2,31 @@
 #include <errno.h>
 #include <sys/socket.h>
 
-int
-initserver(int type, const struct sockaddr *addr, socklen_t alen,
-  int qlen)
+// Figure 16.12 Initialize a socket endpoint for use by a server
+// Figure 16.12 shows a function we can use to allocate and initialize a socket for use by a
+// server process.
+int initserver(int type, const struct sockaddr *addr, 
+                socklen_t alen, int qlen)
 {
-	int fd;
-	int err = 0;
+    int fd;
+    int err = 0;
 
-	if ((fd = socket(addr->sa_family, type, 0)) < 0)
-		return(-1);
-	if (bind(fd, addr, alen) < 0)
-		goto errout;
-	if (type == SOCK_STREAM || type == SOCK_SEQPACKET) {
-		if (listen(fd, qlen) < 0)
-			goto errout;
-	}
-	return(fd);
+    if ((fd = socket(addr->sa_family, type, 0)) < 0)
+        return (-1);
+
+    if (bind(fd, addr, alen) < 0)
+        goto errout;
+
+    if (type == SOCK_STREAM || type == SOCK_SEQPACKET)
+    {
+        if (listen(fd, qlen) < 0)
+            goto errout;
+    }
+    return (fd);
 
 errout:
-	err = errno;
-	close(fd);
-	errno = err;
-	return(-1);
+    err = errno;
+    close(fd);
+    errno = err;
+    return (-1);
 }
