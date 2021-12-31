2ff8de840781e20679e81977f0ccd60a736374cd
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2021-12-31 14:38:58 +0800

    Update initsrv2.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/sockets/initsrv2.c
+++ Platform_dependence/Like_Unix/sockets/initsrv2.c
@@ -2,28 +2,33 @@
 #include <errno.h>
 #include <sys/socket.h>
 
-int
-initserver(int type, const struct sockaddr *addr, socklen_t alen,
-  int qlen)
-{
-	int fd, err;
-	int reuse = 1;
+// Figure 16.22 Initialize a socket endpoint for use by a server with address reuse
 
-	if ((fd = socket(addr->sa_family, type, 0)) < 0)
-		return(-1);
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse,
-	  sizeof(int)) < 0)
-		goto errout;
-	if (bind(fd, addr, alen) < 0)
-		goto errout;
-	if (type == SOCK_STREAM || type == SOCK_SEQPACKET)
-		if (listen(fd, qlen) < 0)
-			goto errout;
-	return(fd);
+int initserver(int type, const struct sockaddr *addr, 
+                socklen_t alen, int qlen)
+{
+    int fd, err;
+    int reuse = 1;
+
+    if ((fd = socket(addr->sa_family, type, 0)) < 0)
+        return (-1);
+
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse,
+                   sizeof(int)) < 0)
+        goto errout;
+
+    if (bind(fd, addr, alen) < 0)
+        goto errout;
+
+    if (type == SOCK_STREAM || type == SOCK_SEQPACKET)
+        if (listen(fd, qlen) < 0)
+            goto errout;
+
+    return (fd);
 
 errout:
-	err = errno;
-	close(fd);
-	errno = err;
-	return(-1);
+    err = errno;
+    close(fd);
+    errno = err;
+    return (-1);
 }
