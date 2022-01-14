2cd13172f1292730d9b1f193adb15c297fa268d9
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-01-14 14:29:47 +0800

    Update spipe.c
    Add more comments.

--- Platform_dependence/Like_Unix/lib/spipe.c
+++ Platform_dependence/Like_Unix/lib/spipe.c
@@ -1,6 +1,10 @@
 #include "apue.h"
 #include <sys/socket.h>
 
+// Figure 17.2 Creating a full-duplex pipe
+// Figure 17.2 shows the fd_pipe function, which uses the socketpair function to
+// create a pair of connected UNIX domain stream sockets.
+
 /*
  * Returns a full-duplex pipe (a UNIX domain socket) with
  * the two file descriptors returned in fd[0] and fd[1].
