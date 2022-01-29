befd67fbe2e4fed9223c8c61c3219d2923a2b9f9
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-01-29 14:00:14 +0800

    Update STR_PTR.c
    Format old styles.

--- ARRAYS/STR_PTR.c
+++ ARRAYS/STR_PTR.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 void show_string(char *string)
- { 
-   while (*string)
-     putchar(*string++);
- }
+{
+    while (*string)
+        putchar(*string++);
+}
 
-int main( void )
- {
-   show_string("Jamsa's 1001 C/C++ Tips");
- }
+int main(void)
+{
+    show_string("Jamsa's 1001 C/C++ Tips");
 
+    return 0;
+}
