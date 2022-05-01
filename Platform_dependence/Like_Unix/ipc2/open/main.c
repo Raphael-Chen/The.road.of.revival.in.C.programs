3d287875d07f2e651ab6b6df0d76b36aa2c60bd7
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-05-01 10:30:09 +0800

    Update main.c
    Format old styles and add more comments.

--- Platform_dependence/Like_Unix/ipc2/open/main.c
+++ Platform_dependence/Like_Unix/ipc2/open/main.c
@@ -1,31 +1,38 @@
-#include	"open.h"
-#include	<fcntl.h>
+#include "open.h"
+#include <fcntl.h>
 
-#define	BUFFSIZE	8192
+#define BUFFSIZE 8192
 
-int
-main(int argc, char *argv[])
+// Figure 17.18 The client main function, version 1
+// The main function (Figure 17.18) is a loop that reads a pathname from standard
+// input and copies the file to standard output. The function calls csopen to contact the
+// open server and return an open descriptor.
+
+int main(int argc, char *argv[])
 {
-	int		n, fd;
-	char	buf[BUFFSIZE], line[MAXLINE];
+    int n, fd;
+    char buf[BUFFSIZE], line[MAXLINE];
 
-	/* read filename to cat from stdin */
-	while (fgets(line, MAXLINE, stdin) != NULL) {
-		if (line[strlen(line) - 1] == '\n')
-			line[strlen(line) - 1] = 0; /* replace newline with null */
+    /* read filename to cat from stdin */
+    while (fgets(line, MAXLINE, stdin) != NULL)
+    {
+        if (line[strlen(line) - 1] == '\n')
+            line[strlen(line) - 1] = 0;            /* replace newline with null */
 
-		/* open the file */
-		if ((fd = csopen(line, O_RDONLY)) < 0)
-			continue;	/* csopen() prints error from server */
+        /* open the file */
+        if ((fd = csopen(line, O_RDONLY)) < 0)
+            continue;                              /* csopen() prints error from server */
 
-		/* and cat to stdout */
-		while ((n = read(fd, buf, BUFFSIZE)) > 0)
-			if (write(STDOUT_FILENO, buf, n) != n)
-				err_sys("write error");
-		if (n < 0)
-			err_sys("read error");
-		close(fd);
-	}
+        /* and cat to stdout */
+        while ((n = read(fd, buf, BUFFSIZE)) > 0)
+            if (write(STDOUT_FILENO, buf, n) != n)
+                err_sys("write error");
 
-	exit(0);
+        if (n < 0)
+            err_sys("read error");
+
+        close(fd);
+    }
+
+    exit(0);
 }
