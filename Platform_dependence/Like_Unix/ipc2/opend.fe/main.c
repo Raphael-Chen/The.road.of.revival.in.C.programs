6dc878acf2a306fe5a124f4bb753872aa93b803d
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-05-14 11:35:14 +0800

    Update main.c
    Format old styles and add more comments for this file.

--- Platform_dependence/Like_Unix/ipc2/opend.fe/main.c
+++ Platform_dependence/Like_Unix/ipc2/opend.fe/main.c
@@ -1,21 +1,27 @@
-#include	"opend.h"
+#include "opend.h"
 
-char	 errmsg[MAXLINE];
-int		 oflag;
-char	*pathname;
+char  errmsg[MAXLINE];
+int   oflag;
+char  *pathname;
 
-int
-main(void)
+// Figure 17.21 The server main function, version 1
+// The main function (Figure 17.21) reads the requests from the client on the fd-pipe
+// (its standard input) and calls the function handle_request.
+
+int main(void)
 {
-	int		nread;
-	char	buf[MAXLINE];
+    int  nread;
+    char buf[MAXLINE];
 
-	for ( ; ; ) {	/* read arg buffer from client, process request */
-		if ((nread = read(STDIN_FILENO, buf, MAXLINE)) < 0)
-			err_sys("read error on stream pipe");
-		else if (nread == 0)
-			break;		/* client has closed the stream pipe */
-		handle_request(buf, nread, STDOUT_FILENO);
-	}
-	exit(0);
+    for (;;)  /* read arg buffer from client, process request */
+    { 
+        if ((nread = read(STDIN_FILENO, buf, MAXLINE)) < 0)
+            err_sys("read error on stream pipe");
+        else if (nread == 0)
+            break; /* client has closed the stream pipe */
+
+        handle_request(buf, nread, STDOUT_FILENO);
+    }
+
+    exit(0);
 }
