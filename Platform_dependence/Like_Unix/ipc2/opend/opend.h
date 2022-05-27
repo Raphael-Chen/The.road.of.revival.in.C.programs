5d5ee49cff7100eba1ae5e5cf569bc127d425aab
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-05-27 11:23:21 +0800

    Update opend.h
    Format old style and add more comments for this file.

--- Platform_dependence/Like_Unix/ipc2/opend/opend.h
+++ Platform_dependence/Like_Unix/ipc2/opend/opend.h
@@ -1,24 +1,27 @@
 #include "apue.h"
 #include <errno.h>
 
-#define	CS_OPEN "/tmp/opend.socket"	/* well-known name */
-#define	CL_OPEN "open"				/* client's request for server */
+// Figure 17.26 The opend.h header, version 2
 
-extern int	 debug;		/* nonzero if interactive (not daemon) */
-extern char	 errmsg[];	/* error message string to return to client */
-extern int	 oflag;		/* open flag: O_xxx ... */
-extern char	*pathname;	/* of file to open for client */
+#define CS_OPEN "/tmp/opend.socket" /* well-known name */
+#define CL_OPEN "open"              /* client's request for server */
 
-typedef struct {	/* one Client struct per connected client */
-  int	fd;			/* fd, or -1 if available */
-  uid_t	uid;
+extern int debug;      /* nonzero if interactive (not daemon) */
+extern char errmsg[];  /* error message string to return to client */
+extern int oflag;      /* open flag: O_xxx ... */
+extern char *pathname; /* of file to open for client */
+
+typedef struct         /* one Client struct per connected client */
+{           
+    int fd;            /* fd, or -1 if available */
+    uid_t uid;
 } Client;
 
-extern Client	*client;		/* ptr to malloc'ed array */
-extern int		 client_size;	/* # entries in client[] array */
+extern Client *client;  /* ptr to malloc'ed array */
+extern int client_size; /* # entries in client[] array */
 
-int		 cli_args(int, char **);
-int		 client_add(int, uid_t);
-void	 client_del(int);
-void	 loop(void);
-void	 handle_request(char *, int, int, uid_t);
+int  cli_args(int, char **);
+int  client_add(int, uid_t);
+void client_del(int);
+void loop(void);
+void handle_request(char *, int, int, uid_t);
