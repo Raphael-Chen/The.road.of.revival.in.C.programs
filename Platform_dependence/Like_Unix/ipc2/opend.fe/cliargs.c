409a81ceddcd98564576895db4f6ecd48229adc3
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-05-27 11:22:33 +0800

    Update cliargs.c
    Format old style and add more comments for this file.

--- Platform_dependence/Like_Unix/ipc2/opend.fe/cliargs.c
+++ Platform_dependence/Like_Unix/ipc2/opend.fe/cliargs.c
@@ -1,18 +1,24 @@
-#include	"opend.h"
+#include "opend.h"
+
+// Figure 17.24 The cli_args function
+// It verifies that the client sent the right number of arguments and stores the pathname and
+// open mode in global variables.
 
 /*
  * This function is called by buf_args(), which is called by
  * handle_request().  buf_args() has broken up the client's
  * buffer into an argv[]-style array, which we now process.
  */
-int
-cli_args(int argc, char **argv)
+int cli_args(int argc, char **argv)
 {
-	if (argc != 3 || strcmp(argv[0], CL_OPEN) != 0) {
-		strcpy(errmsg, "usage: <pathname> <oflag>\n");
-		return(-1);
-	}
-	pathname = argv[1];		/* save ptr to pathname to open */
-	oflag = atoi(argv[2]);
-	return(0);
+    if (argc != 3 || strcmp(argv[0], CL_OPEN) != 0)
+    {
+        strcpy(errmsg, "usage: <pathname> <oflag>\n");
+        return (-1);
+    }
+
+    pathname = argv[1];        /* save ptr to pathname to open */
+    oflag = atoi(argv[2]);
+
+    return (0);
 }
