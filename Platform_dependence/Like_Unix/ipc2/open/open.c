82c99fffffbcc6d81ab23f55744c25974781e5ba
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-05-27 11:22:59 +0800

    Update open.c
    Format old style and add more comments for this file.

--- Platform_dependence/Like_Unix/ipc2/open/open.c
+++ Platform_dependence/Like_Unix/ipc2/open/open.c
@@ -1,38 +1,45 @@
-#include	"open.h"
-#include	<sys/uio.h>		/* struct iovec */
+#include "open.h"
+#include <sys/uio.h> /* struct iovec */
+
+// Figure 17.25 The csopen function, version 2
+// The file open.c does change from Figure 17.19, since we now call cli_conn instead of
+// doing the fork and exec. This is shown in Figure 17.25.
 
 /*
  * Open the file by sending the "name" and "oflag" to the
  * connection server and reading a file descriptor back.
  */
-int
-csopen(char *name, int oflag)
+int csopen(char *name, int oflag)
 {
-	int				len;
-	char			buf[12];
-	struct iovec	iov[3];
-	static int		csfd = -1;
+    int          len;
+    char         buf[12];
+    struct iovec iov[3];
+    static int   csfd = -1;
 
-	if (csfd < 0) {		/* open connection to conn server */
-		if ((csfd = cli_conn(CS_OPEN)) < 0) {
-			err_ret("cli_conn error");
-			return(-1);
-		}
-	}
+    if (csfd < 0)   /* open connection to conn server */
+    { 
+        if ((csfd = cli_conn(CS_OPEN)) < 0)
+        {
+            err_ret("cli_conn error");
+            return (-1);
+        }
+    }
 
-	sprintf(buf, " %d", oflag);		/* oflag to ascii */
-	iov[0].iov_base = CL_OPEN " ";	/* string concatenation */
-	iov[0].iov_len  = strlen(CL_OPEN) + 1;
-	iov[1].iov_base = name;
-	iov[1].iov_len  = strlen(name);
-	iov[2].iov_base = buf;
-	iov[2].iov_len  = strlen(buf) + 1;	/* null always sent */
-	len = iov[0].iov_len + iov[1].iov_len + iov[2].iov_len;
-	if (writev(csfd, &iov[0], 3) != len) {
-		err_ret("writev error");
-		return(-1);
-	}
+    sprintf(buf, " %d", oflag);    /* oflag to ascii */
+    iov[0].iov_base = CL_OPEN " "; /* string concatenation */
+    iov[0].iov_len  = strlen(CL_OPEN) + 1;
+    iov[1].iov_base = name;
+    iov[1].iov_len  = strlen(name);
+    iov[2].iov_base = buf;
+    iov[2].iov_len  = strlen(buf) + 1; /* null always sent */
+    len = iov[0].iov_len + iov[1].iov_len + iov[2].iov_len;
+    
+    if (writev(csfd, &iov[0], 3) != len)
+    {
+        err_ret("writev error");
+        return (-1);
+    }
 
-	/* read back descriptor; returned errors handled by write() */
-	return(recv_fd(csfd, write));
+    /* read back descriptor; returned errors handled by write() */
+    return (recv_fd(csfd, write));
 }
