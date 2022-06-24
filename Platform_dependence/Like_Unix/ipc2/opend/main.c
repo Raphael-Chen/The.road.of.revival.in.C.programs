6a82adb7ff0a1a9a33089d96654be8b9d88fffce
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-06-24 11:30:54 +0800

    Update main.c
    Format old styles and add more comments for this file.

--- Platform_dependence/Like_Unix/ipc2/opend/main.c
+++ Platform_dependence/Like_Unix/ipc2/opend/main.c
@@ -1,32 +1,39 @@
-#include	"opend.h"
-#include	<syslog.h>
+#include "opend.h"
+#include <syslog.h>
 
-int		 debug, oflag, client_size, log_to_stderr;
-char	 errmsg[MAXLINE];
-char	*pathname;
-Client	*client = NULL;
+// Figure 17.28 The server main function, version 2
+// The open server’s main function (Figure 17.28) defines the global variables,
+// processes the command-line options, and calls the function loop. If we invoke the
+// server with the -d option, the server runs interactively instead of as a daemon. This
+// option is used when testing the server.
 
-int
-main(int argc, char *argv[])
+int      debug, oflag, client_size, log_to_stderr;
+char     errmsg[MAXLINE];
+char     *pathname;
+Client   *client = NULL;
+
+int main(int argc, char *argv[])
 {
-	int		c;
+    int c;
 
-	log_open("open.serv", LOG_PID, LOG_USER);
+    log_open("open.serv", LOG_PID, LOG_USER);
 
-	opterr = 0;		/* don't want getopt() writing to stderr */
-	while ((c = getopt(argc, argv, "d")) != EOF) {
-		switch (c) {
-		case 'd':		/* debug */
-			debug = log_to_stderr = 1;
-			break;
+    opterr = 0;            /* don't want getopt() writing to stderr */
+    while ((c = getopt(argc, argv, "d")) != EOF)
+    {
+        switch (c)
+        {
+            case 'd':      /* debug */
+                debug = log_to_stderr = 1;
+                break;
 
-		case '?':
-			err_quit("unrecognized option: -%c", optopt);
-		}
-	}
+            case '?':
+                err_quit("unrecognized option: -%c", optopt);
+        }
+    }
 
-	if (debug == 0)
-		daemonize("opend");
+    if (debug == 0)
+        daemonize("opend");
 
-	loop();		/* never returns */
+    loop();                /* never returns */
 }
