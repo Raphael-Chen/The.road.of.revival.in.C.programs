b91e88e3a97bdf6a3acf7da389e5e6aa72fcd77b
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-06-24 11:31:25 +0800

    Update loop.select.c
    Format old styles and add more comments for this file.

--- Platform_dependence/Like_Unix/ipc2/opend/loop.select.c
+++ Platform_dependence/Like_Unix/ipc2/opend/loop.select.c
@@ -1,59 +1,82 @@
-#include	"opend.h"
-#include	<sys/select.h>
+#include "opend.h"
+#include <sys/select.h>
 
-void
-loop(void)
+// Figure 17.29 The loop function using select
+
+// The function loop is the server’s infinite loop. We’ll show two versions of this
+// function. Figure 17.29 shows one version that uses select; Figure 17.30 shows another
+// version that uses poll.
+
+void loop(void)
 {
-	int		i, n, maxfd, maxi, listenfd, clifd, nread;
-	char	buf[MAXLINE];
-	uid_t	uid;
-	fd_set	rset, allset;
+    int     i, n, maxfd, maxi, listenfd, clifd, nread;
+    char    buf[MAXLINE];
+    uid_t   uid;
+    fd_set  rset, allset;
 
-	FD_ZERO(&allset);
+    FD_ZERO(&allset);
 
-	/* obtain fd to listen for client requests on */
-	if ((listenfd = serv_listen(CS_OPEN)) < 0)
-		log_sys("serv_listen error");
-	FD_SET(listenfd, &allset);
-	maxfd = listenfd;
-	maxi = -1;
+    /* obtain fd to listen for client requests on */
+    listenfd = serv_listen(CS_OPEN);
+    if (listenfd < 0)
+        log_sys("serv_listen error");
 
-	for ( ; ; ) {
-		rset = allset;	/* rset gets modified each time around */
-		if ((n = select(maxfd + 1, &rset, NULL, NULL, NULL)) < 0)
-			log_sys("select error");
+    FD_SET(listenfd, &allset);
+    maxfd = listenfd;
+    maxi = -1;
 
-		if (FD_ISSET(listenfd, &rset)) {
-			/* accept new client request */
-			if ((clifd = serv_accept(listenfd, &uid)) < 0)
-				log_sys("serv_accept error: %d", clifd);
-			i = client_add(clifd, uid);
-			FD_SET(clifd, &allset);
-			if (clifd > maxfd)
-				maxfd = clifd;	/* max fd for select() */
-			if (i > maxi)
-				maxi = i;	/* max index in client[] array */
-			log_msg("new connection: uid %d, fd %d", uid, clifd);
-			continue;
-		}
+    for (;;)
+    {
+        rset = allset;             /* rset gets modified each time around */
 
-		for (i = 0; i <= maxi; i++) {	/* go through client[] array */
-			if ((clifd = client[i].fd) < 0)
-				continue;
-			if (FD_ISSET(clifd, &rset)) {
-				/* read argument buffer from client */
-				if ((nread = read(clifd, buf, MAXLINE)) < 0) {
-					log_sys("read error on fd %d", clifd);
-				} else if (nread == 0) {
-					log_msg("closed: uid %d, fd %d",
-					  client[i].uid, clifd);
-					client_del(clifd);	/* client has closed cxn */
-					FD_CLR(clifd, &allset);
-					close(clifd);
-				} else {	/* process client's request */
-					handle_request(buf, nread, clifd, client[i].uid);
-				}
-			}
-		}
-	}
+        n = select(maxfd + 1, &rset, NULL, NULL, NULL);
+        if (n < 0)
+            log_sys("select error");
+
+        if (FD_ISSET(listenfd, &rset))
+        {
+            /* accept new client request */
+            clifd = serv_accept(listenfd, &uid);
+            if (clifd < 0)
+                log_sys("serv_accept error: %d", clifd);
+            i = client_add(clifd, uid);
+            FD_SET(clifd, &allset);
+
+            if (clifd > maxfd)
+                maxfd = clifd;       /* max fd for select() */
+            if (i > maxi)
+                maxi = i;            /* max index in client[] array */
+            log_msg("new connection: uid %d, fd %d", uid, clifd);
+            continue;
+        }
+
+        for (i = 0; i <= maxi; i++)
+        { /* go through client[] array */
+            clifd = client[i].fd;
+            if (clifd < 0)
+                continue;
+                
+            if (FD_ISSET(clifd, &rset))
+            {
+                /* read argument buffer from client */
+                nread = read(clifd, buf, MAXLINE);
+                if ( nread < 0)
+                {
+                    log_sys("read error on fd %d", clifd);
+                }
+                else if (nread == 0)
+                {
+                    log_msg("closed: uid %d, fd %d",
+                            client[i].uid, clifd);
+                    client_del(clifd);            /* client has closed cxn */
+                    FD_CLR(clifd, &allset);
+                    close(clifd);
+                }
+                else  /* process client's request */
+                { 
+                    handle_request(buf, nread, clifd, client[i].uid);
+                }
+            }
+        }
+    }
 }
