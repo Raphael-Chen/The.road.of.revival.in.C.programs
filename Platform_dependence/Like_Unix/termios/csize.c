afe841750a03b6a3b4997860083f134be12ea95d
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-07-30 15:19:58 +0800

    Create csize.c
    Format old styles and add more comments for this file.

---
+++ Platform_dependence/Like_Unix/termios/csize.c
@@ -0,0 +1,37 @@
+#include "apue.h"
+#include <termios.h>
+
+// Figure 18.11 demonstrates the use of these masks to extract a value and to set a value.
+
+int main(void)
+{
+    struct termios term;
+
+    if (tcgetattr(STDIN_FILENO, &term) < 0)
+        err_sys("tcgetattr error");
+
+    switch (term.c_cflag & CSIZE)
+    {
+        case CS5:
+            printf("5 bits/byte\n");
+            break;
+        case CS6:
+            printf("6 bits/byte\n");
+            break;
+        case CS7:
+            printf("7 bits/byte\n");
+            break;
+        case CS8:
+            printf("8 bits/byte\n");
+            break;
+        default:
+            printf("unknown bits/byte\n");
+    }
+
+    term.c_cflag &= ~CSIZE;   /* zero out the bits */
+    term.c_cflag |= CS8;      /* set 8 bits/byte */
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &term) < 0)
+        err_sys("tcsetattr error");
+
+    exit(0);
+}
