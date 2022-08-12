827afb6277fe39d234bd3a6fefe429128478473f
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-08-12 16:23:49 +0800

    Update t_isatty.c
    Format old styles and add more comments.

--- Platform_dependence/Like_Unix/termios/t_isatty.c
+++ Platform_dependence/Like_Unix/termios/t_isatty.c
@@ -1,10 +1,23 @@
 #include "apue.h"
 
-int
-main(void)
+// Figure 18.14 Test the isatty function
+
+int main(void)
 {
-	printf("fd 0: %s\n", isatty(0) ? "tty" : "not a tty");
-	printf("fd 1: %s\n", isatty(1) ? "tty" : "not a tty");
-	printf("fd 2: %s\n", isatty(2) ? "tty" : "not a tty");
-	exit(0);
+    printf("fd 0: %s\n", isatty(0) ? "tty" : "not a tty");
+    printf("fd 1: %s\n", isatty(1) ? "tty" : "not a tty");
+    printf("fd 2: %s\n", isatty(2) ? "tty" : "not a tty");
+
+    return 0;
 }
+
+
+// $ ./t_isatty
+// fd 0: tty
+// fd 1: tty
+// fd 2: tty
+// 
+// $ ./t_isatty </etc/passwd 2>/dev/null
+// fd 0: not a tty
+// fd 1: tty
+// fd 2: not a tty
