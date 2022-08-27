9a6252924881ef0f2c638463845e2b3ec9ba0881
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-08-27 17:43:21 +0800

    Update t_ttyname.c
    Format old styles and add more comments for this file.

--- Platform_dependence/Like_Unix/termios/t_ttyname.c
+++ Platform_dependence/Like_Unix/termios/t_ttyname.c
@@ -1,35 +1,51 @@
 #include "apue.h"
 
-int
-main(void)
+// Figure 18.16 Test the ttyname function
+
+int main(void)
 {
-	char *name;
-	if (isatty(0)) {
-		name = ttyname(0);
-		if (name == NULL)
-			name = "undefined";
-	} else {
-		name = "not a tty";
-	}
-	printf("fd 0: %s\n", name);
+    char *name;
+    if (isatty(0))
+    {
+        name = ttyname(0);
+        if (name == NULL)
+            name = "undefined";
+    }
+    else
+    {
+        name = "not a tty";
+    }
+    printf("fd 0: %s\n", name);
 
-	if (isatty(1)) {
-		name = ttyname(1);
-		if (name == NULL)
-			name = "undefined";
-	} else {
-		name = "not a tty";
-	}
-	printf("fd 1: %s\n", name);
+    if (isatty(1))
+    {
+        name = ttyname(1);
+        if (name == NULL)
+            name = "undefined";
+    }
+    else
+    {
+        name = "not a tty";
+    }
+    printf("fd 1: %s\n", name);
 
-	if (isatty(2)) {
-		name = ttyname(2);
-		if (name == NULL)
-			name = "undefined";
-	} else {
-		name = "not a tty";
-	}
-	printf("fd 2: %s\n", name);
+    if (isatty(2))
+    {
+        name = ttyname(2);
+        if (name == NULL)
+            name = "undefined";
+    }
+    else
+    {
+        name = "not a tty";
+    }
+    printf("fd 2: %s\n", name);
 
-	exit(0);
+    exit(0);
 }
+
+// Running the program from Figure 18.16 gives us
+// # ./t_ttyname < /dev/console 2> /dev/null
+// fd 0: /dev/console
+// fd 1: /dev/pts/3
+// fd 2: not a tty
