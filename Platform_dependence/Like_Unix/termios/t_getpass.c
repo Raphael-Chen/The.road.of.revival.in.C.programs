548db8f53ac993b4d404fb0572efec04da2c78e3
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-09-10 10:57:53 +0800

    Update t_getpass.c
    Format old styles and add more comments for this file.

--- Platform_dependence/Like_Unix/termios/t_getpass.c
+++ Platform_dependence/Like_Unix/termios/t_getpass.c
@@ -1,19 +1,23 @@
 #include "apue.h"
 
-char	*getpass(const char *);
+char *getpass(const char *);
 
-int
-main(void)
+// Figure 18.18 Call the getpass function
+// The program in Figure 18.18 calls getpass and prints what we enter to let us verify
+// that the ERASE and KILL characters work (as they should in canonical mode).
+
+int main(void)
 {
-	char	*ptr;
+    char *ptr;
 
-	if ((ptr = getpass("Enter password:")) == NULL)
-		err_sys("getpass error");
-	printf("password: %s\n", ptr);
+    if ((ptr = getpass("Enter password:")) == NULL)
+        err_sys("getpass error");
+    printf("password: %s\n", ptr);
 
-	/* now use password (probably encrypt it) ... */
+    /* now use password (probably encrypt it) ... */
 
-	while (*ptr != 0)
-		*ptr++ = 0;		/* zero it out when we're done with it */
-	exit(0);
+    while (*ptr != 0)
+        *ptr++ = 0;           /* zero it out when we're done with it */
+
+    exit(0);
 }
