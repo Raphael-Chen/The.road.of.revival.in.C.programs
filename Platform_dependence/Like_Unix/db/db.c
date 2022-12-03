a78298aac9245b3a0dbb62369038989895eed8e6
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2022-12-03 11:12:46 +0800

    Update db.c
    Format old styles for a part of code .

--- Platform_dependence/Like_Unix/db/db.c
+++ Platform_dependence/Like_Unix/db/db.c
@@ -1,65 +1,66 @@
 #include "apue.h"
 #include "apue_db.h"
-#include <fcntl.h>		/* open & db_open flags */
+#include <fcntl.h>   /* open & db_open flags */
 #include <stdarg.h>
 #include <errno.h>
-#include <sys/uio.h>	/* struct iovec */
+#include <sys/uio.h> /* struct iovec */
 
 /*
  * Internal index file constants.
  * These are used to construct records in the
  * index file and data file.
  */
-#define IDXLEN_SZ	   4	/* index record length (ASCII chars) */
-#define SEP         ':'	/* separator char in index record */
-#define SPACE       ' '	/* space character */
-#define NEWLINE     '\n'	/* newline character */
+#define IDXLEN_SZ 4  /* index record length (ASCII chars) */
+#define SEP ':'      /* separator char in index record */
+#define SPACE ' '    /* space character */
+#define NEWLINE '\n' /* newline character */
 
 /*
  * The following definitions are for hash chains and free
  * list chain in the index file.
  */
-#define PTR_SZ        7	/* size of ptr field in hash chain */
-#define PTR_MAX 9999999	/* max file offset = 10**PTR_SZ - 1 */
-#define NHASH_DEF	 137	/* default hash table size */
-#define FREE_OFF      0	/* free list offset in index file */
-#define HASH_OFF PTR_SZ	/* hash table offset in index file */
+#define PTR_SZ        7      /* size of ptr field in hash chain */
+#define PTR_MAX 9999999      /* max file offset = 10**PTR_SZ - 1 */
+#define NHASH_DEF   137      /* default hash table size */
+#define FREE_OFF      0      /* free list offset in index file */
+#define HASH_OFF PTR_SZ      /* hash table offset in index file */
 
-typedef unsigned long	DBHASH;	/* hash values */
-typedef unsigned long	COUNT;	/* unsigned counter */
+typedef unsigned long DBHASH; /* hash values */
+typedef unsigned long COUNT;  /* unsigned counter */
 
 /*
  * Library's private representation of the database.
  */
-typedef struct {
-  int    idxfd;  /* fd for index file */
-  int    datfd;  /* fd for data file */
-  char  *idxbuf; /* malloc'ed buffer for index record */
-  char  *datbuf; /* malloc'ed buffer for data record*/
-  char  *name;   /* name db was opened under */
-  off_t  idxoff; /* offset in index file of index record */
-			      /* key is at (idxoff + PTR_SZ + IDXLEN_SZ) */
-  size_t idxlen; /* length of index record */
-			      /* excludes IDXLEN_SZ bytes at front of record */
-			      /* includes newline at end of index record */
-  off_t  datoff; /* offset in data file of data record */
-  size_t datlen; /* length of data record */
-			      /* includes newline at end */
-  off_t  ptrval; /* contents of chain ptr in index record */
-  off_t  ptroff; /* chain ptr offset pointing to this idx record */
-  off_t  chainoff; /* offset of hash chain for this index record */
-  off_t  hashoff;  /* offset in index file of hash table */
-  DBHASH nhash;    /* current hash table size */
-  COUNT  cnt_delok;    /* delete OK */
-  COUNT  cnt_delerr;   /* delete error */
-  COUNT  cnt_fetchok;  /* fetch OK */
-  COUNT  cnt_fetcherr; /* fetch error */
-  COUNT  cnt_nextrec;  /* nextrec */
-  COUNT  cnt_stor1;    /* store: DB_INSERT, no empty, appended */
-  COUNT  cnt_stor2;    /* store: DB_INSERT, found empty, reused */
-  COUNT  cnt_stor3;    /* store: DB_REPLACE, diff len, appended */
-  COUNT  cnt_stor4;    /* store: DB_REPLACE, same len, overwrote */
-  COUNT  cnt_storerr;  /* store error */
+typedef struct
+{
+    int idxfd;          // fd for index file 
+    int datfd;          // fd for data file 
+    char *idxbuf;       // malloc'ed buffer for index record 
+    char *datbuf;       // malloc'ed buffer for data record
+    char *name;         // name db was opened under 
+    off_t idxoff;       // offset in index file of index record 
+                        // key is at (idxoff + PTR_SZ + IDXLEN_SZ) 
+    size_t idxlen;      // length of index record 
+                        // excludes IDXLEN_SZ bytes at front of record 
+                        // includes newline at end of index record 
+    off_t datoff;       // offset in data file of data record 
+    size_t datlen;      // length of data record 
+                        // includes newline at end 
+    off_t ptrval;       // contents of chain ptr in index record 
+    off_t ptroff;       // chain ptr offset pointing to this idx record 
+    off_t chainoff;     // offset of hash chain for this index record 
+    off_t hashoff;      // offset in index file of hash table 
+    DBHASH nhash;       // current hash table size 
+    COUNT cnt_delok;    // delete OK 
+    COUNT cnt_delerr;   // delete error 
+    COUNT cnt_fetchok;  // fetch OK 
+    COUNT cnt_fetcherr; // fetch error 
+    COUNT cnt_nextrec;  // nextrec 
+    COUNT cnt_stor1;    // store: DB_INSERT, no empty, appended 
+    COUNT cnt_stor2;    // store: DB_INSERT, found empty, reused 
+    COUNT cnt_stor3;    // store: DB_REPLACE, diff len, appended 
+    COUNT cnt_stor4;    // store: DB_REPLACE, same len, overwrote 
+    COUNT cnt_storerr;  // store error 
 } DB;
 
 /*
