62b6c29366d6f47a3f42ebcedb4a949f67f745d6
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2023-01-01 14:17:03 +0800

    Update t4.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/db/t4.c
+++ Platform_dependence/Like_Unix/db/t4.c
@@ -2,22 +2,26 @@
 #include "apue_db.h"
 #include <fcntl.h>
 
-int
-main(void)
+// Figure 20.3 Create a database and write three records to it
+
+int main(void)
 {
-	DBHANDLE	db;
+    DBHANDLE db;
+    
+    db = db_open("db4", O_RDWR | O_CREAT | O_TRUNC,
+                 FILE_MODE);
+    if ( NULL == db )
+        err_sys("db_open error");
 
-	if ((db = db_open("db4", O_RDWR | O_CREAT | O_TRUNC,
-	  FILE_MODE)) == NULL)
-		err_sys("db_open error");
+    if (db_store(db, "Alpha", "data1", DB_INSERT) != 0)
+        err_quit("db_store error for alpha");
 
-	if (db_store(db, "Alpha", "data1", DB_INSERT) != 0)
-		err_quit("db_store error for alpha");
-	if (db_store(db, "beta", "Data for beta", DB_INSERT) != 0)
-		err_quit("db_store error for beta");
-	if (db_store(db, "gamma", "record3", DB_INSERT) != 0)
-		err_quit("db_store error for gamma");
+    if (db_store(db, "beta", "Data for beta", DB_INSERT) != 0)
+        err_quit("db_store error for beta");
+        
+    if (db_store(db, "gamma", "record3", DB_INSERT) != 0)
+        err_quit("db_store error for gamma");
 
-	db_close(db);
-	exit(0);
+    db_close(db);
+    exit(0);
 }
