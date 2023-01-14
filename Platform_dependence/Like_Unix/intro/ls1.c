d5acc3752476b22330cc8cdd4075c33d56576190
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2023-01-14 11:13:53 +0800

    Update ls1.c
    Format old style and add more comments.

--- Platform_dependence/Like_Unix/intro/ls1.c
+++ Platform_dependence/Like_Unix/intro/ls1.c
@@ -1,18 +1,23 @@
 #include "apue.h"
 #include <dirent.h>
 
+// Chapter 1. UNIX System Overview
+// Figure 1.3 List all the files in a directory
+
 int main(int argc, char *argv[])
 {
-    //  /* This is the data type of directory stream objects.
-    // The actual structure is opaque to users.  */
-    DIR *dp;
+    // This is the data type of directory stream objects.
+    // The actual structure is opaque to users.  
+    DIR           *dp;
     struct dirent *dirp;
 
     if (argc != 2)
         err_quit("usage: ls directory_name");
 
-    if ((dp = opendir(argv[1])) == NULL)
+    dp = opendir(argv[1]);
+    if ( dp == NULL )
         err_sys("can't open %s", argv[1]);
+
     while ((dirp = readdir(dp)) != NULL)
         printf("%s\n", dirp->d_name);
 
