594a3679c435e0d8989ee1e791e6c92d5620df51
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2023-02-04 12:28:46 +0800

    Update testerror.c
    Add more comments.

--- Platform_dependence/Like_Unix/intro/testerror.c
+++ Platform_dependence/Like_Unix/intro/testerror.c
@@ -1,11 +1,15 @@
 #include "apue.h"
 #include <errno.h>
 
+// Figure 1.8 Demonstrate strerror and perror
+
 int main(int argc, char *argv[])
 {
     fprintf(stderr, "EACCES: %s\n", strerror(EACCES)); //#define EACCES 13	/* Permission denied */
     errno = ENOENT;                                    //#define ENOENT 2 /* No such file or directory */
     perror(argv[0]);
 
-    exit(0);
+    return 0;
 }
+
+// Figure 1.8 shows the use of these two error functions.
