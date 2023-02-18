a7be4bc51a391d846278097ef02675658662ca45
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2023-02-18 11:23:16 +0800

    Update hello.c
    Add more comments.

--- Platform_dependence/Like_Unix/intro/hello.c
+++ Platform_dependence/Like_Unix/intro/hello.c
@@ -1,7 +1,17 @@
 #include "apue.h"
 
+// The program in Figure 1.6 prints its process ID.
+
 int main(void)
 {
     printf("hello world from process ID %ld\n", (long)getpid());
+
     exit(0);
 }
+
+// If we compile this program into the file a.out and execute it, we have
+// $ ./a.out
+// hello world from process ID 851
+// $ ./a.out
+// hello world from process ID 854
+
