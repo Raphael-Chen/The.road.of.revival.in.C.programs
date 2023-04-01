cd76212f99ac73509a117f378cd29cc9110a110e
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2023-04-01 10:56:42 +0800

    Update hello.c
    With return to replace the exit function.

--- Platform_dependence/Like_Unix/intro/hello.c
+++ Platform_dependence/Like_Unix/intro/hello.c
@@ -6,7 +6,7 @@ int main(void)
 {
     printf("hello world from process ID %ld\n", (long)getpid());
 
-    exit(0);
+    return 0;
 }
 
 // If we compile this program into the file a.out and execute it, we have
