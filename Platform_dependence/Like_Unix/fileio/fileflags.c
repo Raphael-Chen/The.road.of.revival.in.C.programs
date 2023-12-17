763d1a158e5de37f8273eedca690124c94710777
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2023-12-17 10:36:37 +0800

    Update fileflags.c
    Modify some codes and add more comments

--- Platform_dependence/Like_Unix/fileio/fileflags.c
+++ Platform_dependence/Like_Unix/fileio/fileflags.c
@@ -1,7 +1,7 @@
 #include "apue.h"
 #include <fcntl.h>
 
-// Figure 3.10. Print file flags for specified descriptor
+// Figure 3.11 Print file flags for specified descriptor
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
     if (argc != 2)
         err_quit("usage: a.out <descriptor#>");
 
-    if ((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0)
+    val = fcntl(atoi(argv[1]), F_GETFL, 0);  // Get/set file status flags (cmd = F_GETFL or F_SETFL)
+    if ( val < 0 )
         err_sys("fcntl error for fd %d", atoi(argv[1]));
 
     switch (val & O_ACCMODE)
@@ -44,5 +45,17 @@ int main(int argc, char *argv[])
 #endif
 
     putchar('\n');
-    exit(0);
+    return 0;
 }
+
+/******
+$ ./fileflags 0 < /dev/tty
+read only
+$ ./fileflags 1 >temp.foo
+$ cat temp.foo
+write only
+$ ./fileflags 2 2>>temp.foo
+write only, append
+$ ./fileflags 5 5<>temp.foo
+read write
+******/
