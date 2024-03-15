febb0e0f3b5406c6bd27a4d96203e81a9ba8a142
Christopher_glb <getaGoodoffer_glb@evergrand.com>
2024-03-15 20:05:43 +0800

    Update setfl.c
    Format the old styles and add more notes.

--- Platform_dependence/Like_Unix/fileio/setfl.c
+++ Platform_dependence/Like_Unix/fileio/setfl.c
@@ -8,10 +8,13 @@ void set_fl(int fd, int flags)
 {
     int val;
 
-    if ((val = fcntl(fd, F_GETFL, 0)) < 0)
+    val = fcntl(fd, F_GETFL, 0);
+    if ( val < 0 )
         err_sys("fcntl F_GETFL error");
 
-    val |= flags; /* turn on flags */
+    /* turn on flags */
+    val |= flags;
+    // val &= ~flags; /* turn flags off */
 
     if (fcntl(fd, F_SETFL, val) < 0)
         err_sys("fcntl F_SETFL error");
